6f29c53a5e132facf0faeddd270a8106640d12da
Apostol123 <apostol516@gmail.com>
2020-08-03 08:56:44 +0100

    update pod version create titleLabel remove duplicated func

--- Autoescuela/Autoescuela/Common/Coordinator.swift
+++ Autoescuela/Autoescuela/Common/Coordinator.swift
@@ -18,13 +18,3 @@ extension String {
         return NSLocalizedString(self, comment: "")
     }
 }
-
-extension UIView {
-    func fill(view : UIView) {
-
-        leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    }
-}
--- Autoescuela/Autoescuela/Modules/Register/RegisterViewController.swift
+++ Autoescuela/Autoescuela/Modules/Register/RegisterViewController.swift
@@ -7,11 +7,34 @@
 //
 
 import UIKit
+import AutoEscuelaComponents
 
 class RegisterViewController: UIViewController {
     
+    lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    lazy var titleLabel: TitleLabel = {
+        let titleLabel = TitleLabel(frame: .zero)
+        titleLabel.configure(text: "Registrarse")
+        return titleLabel
+    }()
+    
+    lazy var titleLabelContainer: UIView = {
+        let view = UIView(frame: .zero)
+        view.fill(view: titleLabel, edgeInset: UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 16))
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        view.fill(view: mainStackView)
+        mainStackView.addArrangedSubview(titleLabelContainer)
     }
     
     override func viewDidAppear(_ animated: Bool) {
